d444995d485340922ecaa1150dd8e7c1b36e902d
Siul <kksu149@naver.com>
2025-07-14 16:39:13 +0900

    [FIX] 테스트 값 추가했습니다 시간대 2시간, 3시간 roomkey 3개, 4개 날짜 변경 16일, 17일

--- tests/test_groove_service.py
+++ tests/test_groove_service.py
@@ -7,12 +7,12 @@ from service.groove_checker import get_groove_availability
 async def test_get_groove_availability():
 
     # 실제 테스트용 파라미터를 여기에 입력하세요
-    date = "2025-07-15"
-    hour_slots = ["21:00", "23:00"]
+    date = "2025-07-16"
+    hour_slots = ["20:00", "23:00"]
     groove_rooms = [
-        RoomKey(name="A룸", branch="그루브 사당점", business_id="sadang", biz_item_id="groove-A"),
-        RoomKey(name="B룸", branch="그루브 사당점", business_id="sadang", biz_item_id="groove-B"),
-        RoomKey(name="C룸", branch="그루브 사당점", business_id="sadang", biz_item_id="groove-C")
+        RoomKey(name="A룸", branch="그루브 사당점", business_id="sadang", biz_item_id="13"),
+        RoomKey(name="B룸", branch="그루브 사당점", business_id="sadang", biz_item_id="14"),
+        RoomKey(name="C룸", branch="그루브 사당점", business_id="sadang", biz_item_id="15")
     ]
 
     result = await get_groove_availability(date, hour_slots, groove_rooms)
@@ -25,10 +25,10 @@ async def test_get_groove_availability1():
     date = "2025-07-16"
     hour_slots = ["21:00", "23:00"]
     groove_rooms = [
-        RoomKey(name="A룸", branch="그루브 사당점", business_id="sadang", biz_item_id="groove-A"),
-        RoomKey(name="B룸", branch="그루브 사당점", business_id="sadang", biz_item_id="groove-B"),
-        RoomKey(name="C룸", branch="그루브 사당점", business_id="sadang", biz_item_id="groove-C"),
-        RoomKey(name="D룸", branch="그루브 사당점", business_id="sadang", biz_item_id="groove-D")
+        RoomKey(name="A룸", branch="그루브 사당점", business_id="sadang", biz_item_id="13"),
+        RoomKey(name="B룸", branch="그루브 사당점", business_id="sadang", biz_item_id="14"),
+        RoomKey(name="C룸", branch="그루브 사당점", business_id="sadang", biz_item_id="15"),
+        RoomKey(name="D룸", branch="그루브 사당점", business_id="sadang", biz_item_id="16")
     ]
 
     result = await get_groove_availability(date, hour_slots, groove_rooms)
@@ -41,9 +41,9 @@ async def test_get_groove_availability2():
     date = "2025-07-17"
     hour_slots = ["21:00", "23:00"]
     groove_rooms = [
-        RoomKey(name="A룸", branch="그루브 사당점", business_id="sadang", biz_item_id="groove-A"),
-        RoomKey(name="B룸", branch="그루브 사당점", business_id="sadang", biz_item_id="groove-B"),
-        RoomKey(name="C룸", branch="그루브 사당점", business_id="sadang", biz_item_id="groove-C")
+        RoomKey(name="A룸", branch="그루브 사당점", business_id="sadang", biz_item_id="13"),
+        RoomKey(name="B룸", branch="그루브 사당점", business_id="sadang", biz_item_id="14"),
+        RoomKey(name="C룸", branch="그루브 사당점", business_id="sadang", biz_item_id="15")
     ]
 
     result = await get_groove_availability(date, hour_slots, groove_rooms)

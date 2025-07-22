32c33f9a0d21375e57d020a78f87fc9bcd3f4e4a
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-07-22 14:04:29 +0900

    [FEAT] exception핸들러 추가

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from api.available_room import router as available_router
+from exception import custom_exception_handler
+from exception.common_exception import BaseCustomException
 
 app = FastAPI()
 app.include_router(available_router)
+
+app.add_exception_handler(BaseCustomException, custom_exception_handler)

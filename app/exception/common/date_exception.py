8bbb3bb874b08978ea3a1a7b0f6017c1f423d612
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-08-05 16:25:13 +0900

    FIX: date&hour 형식안맞는거 422로 변경

--- app/exception/common/date_exception.py
+++ app/exception/common/date_exception.py
@@ -5,7 +5,7 @@ class InvalidDateFormatError(BaseCustomException):
     """날짜 형식이 잘못된 경우"""
     error_code = "Date-001"
     message = "날짜 형식이 잘못되었습니다."
-    status_code = 400
+    status_code = 422
 
 class PastDateNotAllowedError(BaseCustomException):
     """과거 날짜는 허용되지 않을 때 발생"""
--- app/exception/common/hour_excpetion.py
+++ app/exception/common/hour_excpetion.py
@@ -6,10 +6,12 @@ class InvalidHourSlotError(BaseCustomException):
     """시간 형식이 잘못된 경우"""
     error_code = "Hour-001"
     message = "시간 형식이 잘못되었습니다."
-    status_code = 400
+    status_code = 422
 
 class PastHourSlotNotAllowedError(BaseCustomException):
     """과거 시간은 허용되지 않을 때 발생"""
     error_code = "Hour-002"
     message = "과거 시간은 허용되지 않습니다."
     status_code = 400
+
+# 근데 여기에 시간슬롯 연속
